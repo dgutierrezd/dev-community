bfc2b15c0b4b8d95b70d083f7c1875e7efd9327c
Daniel Gutierrez <danielgudu22@gmail.com>
2019-07-16 22:02:32 -0500

    jwt auth with passport

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://daniel:daniel1@ds145926.mlab.com:45926/heroku_rnt5ltbw"
+  mongoURI: "mongodb://daniel:daniel1@ds145926.mlab.com:45926/heroku_rnt5ltbw",
+  secretOrKey: "secret"
 };
