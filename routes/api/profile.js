a4b3c6d9f04e59b7745c2180f447376170a25a33
Daniel Gutierrez <danielgudu22@gmail.com>
2019-07-18 13:04:58 -0500

    Delete user and his profile.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -10,7 +10,7 @@ const validateEducationInput = require('../../validation/education');
 
 // Load Profile Model
 const Profile = require('../../models/Profile');
-// Load User Profile
+// Load User Model
 const User = require('../../models/User');
 
 // @route  GET api/profile/test
@@ -254,6 +254,18 @@ router.delete('/education/:edu_id', passport.authenticate('jwt', {session: false
       profile.save().then(profile => res.json(profile));
     })
     .catch(err => res.status(404).json(err));
+})
+
+// @route  DELETE api/profile/
+// @desc   Delete user and profile
+// @access Private
+router.delete('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+  Profile.findOneAndRemove({ user:req.user.id })
+    .then( () => {
+      User.findByIdAndRemove({ _id: req.user.id })
+        .then( () => res.json({success: true}))
+    })
+    
 })
 
 module.exports = router;
