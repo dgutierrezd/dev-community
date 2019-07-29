d9dad19d7ab547c00c61042213fe3f4d11a4129c
Daniel Gutierrez <danielgudu22@gmail.com>
2019-07-29 12:54:25 -0500

    Deploying Finish

--- client/src/store.js
+++ client/src/store.js
@@ -7,7 +7,8 @@ const initialState = {};
 const middleware = [thunk]
 
 const store = createStore(
-    rootReducer, initialState, compose(applyMiddleware(...middleware))
+    rootReducer, initialState, compose(applyMiddleware(...middleware), 
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f)
 );
 
 export default store;
